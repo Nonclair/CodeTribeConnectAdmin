adff77da006a92b8e30990bdbe95a51eaf8ec64e
nonclair <nonhlanhlamokoena11@gmail.com>
2018-01-19 13:51:57 +0200

    commit

--- app/src/main/java/com/mlab/nonhlanhla/codetribeconnectadmin/TribeList.java
+++ app/src/main/java/com/mlab/nonhlanhla/codetribeconnectadmin/TribeList.java
@@ -1,7 +1,11 @@
 package com.mlab.nonhlanhla.codetribeconnectadmin;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +15,17 @@ public class TribeList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tribe_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.profile_list_back_fab_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
@@ -28,7 +43,7 @@ public class TribeList extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.btnLogout) {
             return true;
         }
 
